bfe9df4702bf5f257526e7074d8f5fb8ab400198
Montana Mendy <montana@linux.com>
2019-12-16 18:09:15 -0800

    using "use-strict" method, this is a rare occasion where I would

---
+++ Instagram.js
@@ -0,0 +1,29 @@
+'use-strict';
+
+import React from 'react';
+import Instafeed from 'instafeed.js';
+
+export default class Instagram extends React.Component {
+
+    componentDidMount() {
+
+        new Instafeed({
+            get: 'user',
+            userId: '7248375',
+            accessToken: 'null',
+            resolution: 'standard_resolution',
+            limit: '4',
+            template: '<a href="{{link}}"><img style="height:auto; width:50%;" src="{{image}}"></a>'
+        }).run();
+    }
+
+        // this links to Pearl Charles instagram, and is formatted
+
+    
+    render() {
+
+        return (
+            <div id="instafeed"></div>
+        );
+    }
+}
