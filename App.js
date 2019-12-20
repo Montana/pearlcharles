a5d1bd88e059237dbd0367dc584895745bd86fd0
Montana Mendy <montana@linux.com>
2019-12-19 19:51:19 -0800

    chore: upgrade to jest 24

---
+++ App.js
@@ -0,0 +1,26 @@
+'use-strict';
+
+import React from 'react';
+import { Helmet } from 'react-helmet;'
+import Navigation from './components/Navigation.js';
+import Container from './components/Container.js';
+import Footer from './components/Footer.js';
+
+class App extends React.Component {
+
+    render(){
+
+        return (
+
+            <div>
+                <Navigation />
+                <Container />
+                <Footer />
+            </div>
+        );
+    }
+}
+
+export default App;
+
+// The Instagram component in the PearlCharles directory will pull data from Instagram in the render method using Axios, needs implementation here
